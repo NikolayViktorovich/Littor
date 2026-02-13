a2d0c82ba884511ad62d7828757362bcb7f130b8
NIKOLAY VIKTOROVICH <145424511+NikolayViktorovich@users.noreply.github.com>
2026-02-14 00:39:42 +0500

    Update App.js main component

--- App.js
+++ App.js
@@ -1,33 +1,18 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AppProvider } from './src/context/AppContext';
 import AppNavigator from './src/navigation/AppNavigator';
-import * as Font from 'expo-font';
-import {
-  Manrope_300Light,
-  Manrope_400Regular,
-  Manrope_500Medium,
-  Manrope_600SemiBold,
-  Manrope_700Bold,
-  Manrope_800ExtraBold,
-} from '@expo-google-fonts/manrope';
+import { useFonts, Inter_300Light, Inter_400Regular, Inter_500Medium, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter';
 import { View, ActivityIndicator } from 'react-native';
 export default function App() {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
-  useEffect(() => {
-    async function loadFonts() {
-      await Font.loadAsync({
-        'Manrope-Light': Manrope_300Light,
-        'Manrope-Regular': Manrope_400Regular,
-        'Manrope-Medium': Manrope_500Medium,
-        'Manrope-SemiBold': Manrope_600SemiBold,
-        'Manrope-Bold': Manrope_700Bold,
-        'Manrope-ExtraBold': Manrope_800ExtraBold,
-      });
-      setFontsLoaded(true);
-    }
-    loadFonts();
-  }, []);
+  const [fontsLoaded] = useFonts({
+    'Inter-Light': Inter_300Light,
+    'Inter-Regular': Inter_400Regular,
+    'Inter-Medium': Inter_500Medium,
+    'Inter-SemiBold': Inter_600SemiBold,
+    'Inter-Bold': Inter_700Bold,
+    'Inter-ExtraBold': Inter_800ExtraBold,
+  });
   if (!fontsLoaded) {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#0a0a0a' }}>
